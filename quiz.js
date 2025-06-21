bd37141ebf8cf66281befef3819554db932a7ad9
Victor Nwachukwu <vixkudos@gmail.com>
2025-06-21 18:14:24 +0100

    initial commit for ALX simple quiz

---
+++ quiz.js
@@ -0,0 +1,18 @@
+function checkAnswer() {
+    const correctAnswer = "4";
+
+    const selectedOption = document.querySelector('input[name = "quiz"], checked');
+
+    const userAnswer = selectedOption.value;
+
+    const feedbackElement = document.getElementById("feedback");
+
+    if (userAnswer === correctAnswer) {
+        feedbackElement.textContent = "Correct! Well done.";
+    } else {
+        feedbackElement.textContent = "That's incorrect. Try again!";
+    }
+}
+
+const submitButton = document.getElementById("submit-answer");
+submitButton.addEventListener("click", checkAnswer);
